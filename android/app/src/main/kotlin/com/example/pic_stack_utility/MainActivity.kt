52724ebaebeaab1ea0e8b4d36c1e6495d7221010
Akshay Kumar <akshaymain95@gmail.com>
2022-08-08 20:32:13 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pic_stack_utility/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pic_stack_utility
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/profile_image_holder.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ProfileImage extends StatelessWidget {
+  final String imageUrl;
+  final double rotationAngle;
+  final double opacity;
+
+  const ProfileImage({
+    Key? key,
+    required this.imageUrl,
+    required this.rotationAngle,
+    this.opacity = 1.0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: rotationAngle,
+      child: Opacity(
+        opacity: opacity,
+        child: Container(
+            width: 250,
+            height: 250,
+            decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                border: Border.all(color: Colors.white, width: 5),
+                borderRadius: BorderRadius.circular(5),
+                boxShadow: [
+                  BoxShadow(
+                      color: Theme.of(context).shadowColor,
+                      offset: const Offset(5, 5),
+                      blurRadius: 20.0,
+                      spreadRadius: 2.0),
+                  const BoxShadow(
+                      color: Colors.white,
+                      offset: Offset.zero,
+                      blurRadius: 0.0,
+                      spreadRadius: 0.0)
+                ]),
+            child: Image.network(imageUrl)),
+      ),
+    );
+  }
+}
